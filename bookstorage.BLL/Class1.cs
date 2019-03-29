5a0375daffcd28fbdb93deafa1c41900c9569626
Lucas Bruno <lucasbrunoferreira@gmail.com>
2019-03-28 23:37:21 -0300

    Create the application structure

---
+++ bookstorage.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bookstorage.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ bookstorage.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bookstorage.DAL
+{
+    public class Class1
+    {
+    }
+}
