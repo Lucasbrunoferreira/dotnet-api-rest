a5e7c75dcbba9ac5ac8edec9d93a4fde1f9a92b2
Lucas <lucas.bruno@cedrotech.com>
2019-03-29 14:22:16 -0300

    Added relanships

--- bookstorage.DAL/Databases/PGSQLContext.cs
+++ bookstorage.DAL/Databases/PGSQLContext.cs
@@ -6,6 +6,10 @@ namespace bookstorage.DAL.Databases
 {
   public class PGSQLContext : DbContext
   {
+    public DbSet<Author> Author {get; set;}
+    public DbSet<Book> Book {get; set;}
+    public DbSet<BookGenre> BookGenre {get; set;}
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("PGSQL_CONNECTION_STRING") ?? "host=172.17.0.2;port=5432;database=Teste;user id=postgres;password=mysecretpassword");
@@ -17,6 +21,12 @@ namespace bookstorage.DAL.Databases
       modelBuilder.Entity<Author>().HasKey(a => a.authorId);
       modelBuilder.Entity<BookGenre>().HasKey(bg => bg.genreId);
       modelBuilder.Entity<Book>().HasKey(bg => bg.bookId);
+      modelBuilder.Entity<BookGenre>().HasMany(a => a.books)
+        .WithOne(b => b.genre)
+        .HasForeignKey(c => c.genreId);
+      modelBuilder.Entity<Author>().HasMany(a => a.books)
+        .WithOne(b => b.author)
+        .HasForeignKey(c => c.authorId);
     }
   }
 }
